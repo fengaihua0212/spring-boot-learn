a8bd8e170b78e82b445a4ae0f00cf55db73cf256
fengaihua0212 <fengaihua0212@gmail.com>
2020-05-15 14:42:38 +0800

    quartz demo

---
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -0,0 +1,47 @@
+package com.example.springboot.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fengaihua
+ * @project springboot
+ * @package com.example.springboot.controller
+ * @date 2019/12/23 17:07
+ * @description
+ */
+@RestController
+@RequestMapping("/hello")
+@Api(tags = "hello API 接口")
+public class HelloController {
+    private static final ThreadLocal<Integer> currentUser = ThreadLocal.withInitial(() -> null);
+
+    @GetMapping("say")
+    @ApiOperation(value = "say", notes = "say test")
+    public String say(String name, int age) {
+        return "hello world!";
+    }
+
+    @GetMapping("/wrong")
+    public Map wrong(@RequestParam("userId") Integer userId) {
+        // 设置用户信息前先查询一次ThreadLocal的用户信息
+        String before = Thread.currentThread().getName() + ":" + currentUser.get();
+        // 设置用户信息到ThreadLocal
+        currentUser.set(userId);
+        // 设置用户之后再查询一次ThreadLocal的用户信息
+        String after = Thread.currentThread().getName() + ":" + currentUser.get();
+        // 输出两次查询的结果
+        Map result = new HashMap();
+        result.put("before", before);
+        result.put("after", after);
+
+        return result;
+    }
+}
