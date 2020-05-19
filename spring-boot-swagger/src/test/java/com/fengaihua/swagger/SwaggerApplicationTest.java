940518fd9b6ef3046eb9a22efd78410dd416ad13
fengaihua0212 <fengaihua0212@gmail.com>
2020-05-19 11:47:10 +0800

    增加test目录

---
+++ spring-boot-swagger/src/test/java/com/fengaihua/swagger/SwaggerApplicationTest.java
@@ -0,0 +1,17 @@
+package com.fengaihua.swagger;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author fengaihua
+ * @project spring-boot-learn
+ * @package com.fengaihua.swagger
+ * @date 2020/5/19 11:46
+ * @description
+ */
+@SpringBootTest
+class SwaggerApplicationTest {
+
+}
